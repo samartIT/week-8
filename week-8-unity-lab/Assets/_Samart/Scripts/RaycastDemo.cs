bc559bb6c9914eebd0c3cf30422e52f1cf24643b
samartIT <41602825+samartIT@users.noreply.github.com>
2019-03-06 16:17:17 +0700

    complete all scenes

---
+++ week-8-unity-lab/Assets/_Samart/Scripts/RaycastDemo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDemo : MonoBehaviour
+{
+
+    float rotAroundX, rotAroundY;
+    
+    void Update()
+    {
+        rotAroundY = Input.GetAxis("Mouse X");
+        rotAroundX = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotAroundX, -rotAroundY, 0);
+        DestroyObject();    // Call Destroy function
+    }
+
+    void DestroyObject()
+    {
+        // Check if Raycast found any hit object in the direction of
+        // Camera pointing forward. Found and Destroy.
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            print(hit.collider.name + " has been destroyed.");
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
