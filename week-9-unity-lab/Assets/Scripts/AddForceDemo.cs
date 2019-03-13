247b3faac77e8e48e031ea551f5146d9b5cb944f
PeetTharathanakul <peet12131424@gmail.com>
2019-03-13 11:50:35 +0700

    F.T.

---
+++ week-9-unity-lab/Assets/Scripts/AddForceDemo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 pushDirection = new Vector3(0, 0, 1);
+    public float pushForce = 500;
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddForce(pushDirection * pushForce);
+    }
+}
---
+++ week-9-unity-lab/Assets/Scripts/AddTorqueDemo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTorqueDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float torqueForce = 100;
+    public Vector3 spinDirection = new Vector3(0, 1, 0);
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddTorque(spinDirection * torqueForce);
+    }
+}
