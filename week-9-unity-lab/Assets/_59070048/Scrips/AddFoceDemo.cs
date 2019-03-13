6190194c300f6dadbd56ac698e0686e8fc6add46
Nattakit-Amonjarussang <59070048@kmitl.ac.th>
2019-03-13 12:15:30 +0700

    Scene2

---
+++ week-9-unity-lab/Assets/_59070048/Scrips/AddFoceDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AddFoceDemo : MonoBehaviour
+{
+    public Vector3 pushDirection = new Vector3(0, 0, 1);
+    public float pushForce = 500;
+    private void OnMouseOver()
+    {
+       GetComponent<Rigidbody>().AddForce(pushDirection * pushForce);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
