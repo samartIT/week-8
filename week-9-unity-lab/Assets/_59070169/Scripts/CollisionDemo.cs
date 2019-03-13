fd992645cec94da9d956ea048e2b5b089018156a
ITSupapitch <59070169@it.kmitl.ac.th>
2019-03-13 12:10:35 +0700

    Collision_Trigger_1_labwk9

---
+++ week-9-unity-lab/Assets/_59070169/Scripts/CollisionDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enter: " + collision.collider.name);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
