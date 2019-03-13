8e8b432b392907c623be38ab25e247d1bbe853d4
The buddy_G's <59070144@it.kmitl.ac.th>
2019-03-13 14:55:30 +0700

    Scene 5 & Build
    สร้าง Scene 5 โดยมี Sphere 4 ชิ้น และมีจุดรังสี 1 จุด เมื่อจุดไปโดนกับ Sphere ชิ้นไหน ชิ้นนั้นจะหายไป พร้อม Build ให้เปิดในเว็บและออกมาเป็นเกมได้

---
+++ week-9-unity-lab/Assets/Script/RaycastDemo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDemo : MonoBehaviour
+{
+    float rotAroundX, rotAroundY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotAroundX = Input.GetAxis("Mouse X");
+        rotAroundY = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotAroundX, -rotAroundY, 0);
+        DestroyObject();
+    }
+
+    void DestroyObject()
+    {
+        // Check if Raycast found any hit object in the direction of
+        // camera pointing forward. Found and Destroy.
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            print(hit.collider.name + "has been destroyed.");
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
