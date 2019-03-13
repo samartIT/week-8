d8c3630f1e77e1ea2c33ce168bc55eefe628f561
Thanpisit Wattanasomvong <59070079@it.kmitl.ac.th>
2019-03-13 11:21:17 +0700

    commit

---
+++ week-9-unity-lab/Assets/_59070079/Script/AddForceDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceDemo : MonoBehaviour
+    
+{
+    public Vector3 pushDirecrion = new Vector3(0,0,1);
+    public float pushForce = 500;
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddForce(pushDirecrion * pushForce);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
