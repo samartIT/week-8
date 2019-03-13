520ca1454b49b835ffc881efec174d8267857663
OKKachasom <59070001@kmitl.ac.th>
2019-03-13 11:42:32 +0700

    scene 1

---
+++ week-9-unity-lab/Assets/Scripts/AddForceDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public Vector3 pushDirecion = new Vector3(0,0,1);
+		public float pushForce = 500;
+		public void OnMouseOver()
+		{
+			GetComponent<Rigidbody>().AddForce(pushDirecion * pushForce);
+		}
+        
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
