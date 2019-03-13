0d27b3895caeea3034381d21a086b922c6cfc234
Worapong <59070152@it.kmitl.ac.th>
2019-03-13 11:32:51 +0700

    add scene1-3

---
+++ week-9-unity-lab/Assets/_59070152/Scripts/AddForceDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceDemo : MonoBehaviour
+{
+    public Vector3 pushDirection = new Vector3(0, 0, 1);
+    public float pushForce = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddForce(pushDirection * pushForce);
+    }
+}
---
+++ week-9-unity-lab/Assets/_59070152/Scripts/AddTorqueDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTorqueDemo : MonoBehaviour
+{
+    public float torqueForce = 100;
+    public Vector3 spinDirection = new Vector3(0, 1, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddTorque(spinDirection * torqueForce);
+    }
+}
