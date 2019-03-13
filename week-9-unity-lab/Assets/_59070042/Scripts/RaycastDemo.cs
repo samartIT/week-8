dc54f6ef283208992f0fa698d618a1873858ad41
france14331 <59070042@it.kmitl.ac.th>
2019-03-13 14:39:07 +0700

    Scene5_Raycasting

---
+++ week-9-unity-lab/Assets/_59070042/Scripts/RaycastDemo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDemo : MonoBehaviour
+{
+    float rotAroundX, rotAroundY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotAroundY = Input.GetAxis("Mouse X");
+        rotAroundX = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotAroundX, -rotAroundY, 0);
+        DestroyObject();
+    }
+
+    void DestroyObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            print(hit.collider.name + "has been destroyed.");
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
