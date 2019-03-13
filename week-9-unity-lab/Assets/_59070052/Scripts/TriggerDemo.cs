6a7a93bdb35b66afc275cd963848121fe91bebee
59070052 <59070052@kmitl.ac.th>
2019-03-13 14:23:22 +0700

    trigger

---
+++ week-9-unity-lab/Assets/_59070052/Scripts/TriggerDemo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDemo : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        print("Trigger Enter: " + other.name);
+        if(other.name == "Cube2")
+        {
+            Vector3 rotAng = new Vector3(0, 0, 0);
+            rotAng.y += 0.1f;
+            other.gameObject.transform.Rotate(rotAng);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
