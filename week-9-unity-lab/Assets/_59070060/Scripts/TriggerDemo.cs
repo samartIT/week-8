4d0befce4a84cdfd50b938955753ea241a94659f
Thatsarat <59070060@it.kmitl.ac.th>
2019-03-13 14:14:52 +0700

    Scence 4

---
+++ week-9-unity-lab/Assets/_59070060/Scripts/TriggerDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDemo : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        print("Trigger Enter: " + other.name);
+        if (other.name == "Cube2")
+        {
+            Vector3 rotAng = new Vector3(0, 0, 0);
+            rotAng.y += 1f;
+            other.gameObject.transform.Rotate(rotAng);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
