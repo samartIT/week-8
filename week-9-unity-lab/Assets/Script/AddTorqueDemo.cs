d033dd99ed1806dc9ac46a3f2bbe690dfa18450e
Chanikarn <59070034@kmitl.ac.th>
2019-03-13 23:15:35 +0700

    WK9 - lab 2

---
+++ week-9-unity-lab/Assets/Script/AddTorqueDemo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTorqueDemo : MonoBehaviour
+{
+    public float torqueForce = 100;
+    public Vector3 spinDirection = new Vector3(0, 1, 0);
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddTorque(spinDirection * torqueForce);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
