183997d720e0f1ff3eff566d28a427bf807a06ce
Chanikarn <59070034@kmitl.ac.th>
2019-03-14 22:45:30 +0700

    WK9 - lab 5

---
+++ week-9-unity-lab/Assets/Script/CollisionDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enter: " + collision.collider.name);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
