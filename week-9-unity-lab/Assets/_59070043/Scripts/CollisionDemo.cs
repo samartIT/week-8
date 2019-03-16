24e07fe5e759fc1ff70aa0f94633bbeccfcb8eb0
ynhof6 <59070043@it.kmitl.ac.th>
2019-03-16 23:10:32 +0700

    Finish Scene1-5

---
+++ week-9-unity-lab/Assets/_59070043/Scripts/CollisionDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enter: " + collision.collider.name);
+        if (collision.collider.name != "Cube3")
+            Destroy(collision.collider.gameObject);
+    }
+}
---
+++ week-9-unity-lab/Assets/_59070043/Scripts/RaycastDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDemo : MonoBehaviour
+{
+    float rotAroundX, rotAroundY;
+
+    void Update()
+    {
+        rotAroundX = Input.GetAxis("Mouse X");
+        rotAroundY = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotAroundX, -rotAroundY, 0);
+        DestroyObject();
+    }
+
+    void DestroyObject()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            print(hit.collider.name + "has been destroyed.");
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
