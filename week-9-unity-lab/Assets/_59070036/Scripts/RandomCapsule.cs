c034af8dc88741de619c373458190e8ba8ef8382
Munawang <59070036@it.kmitl.ac.th>
2019-03-17 23:00:27 +0700

    test destroy and score

--- week-9-unity-lab/Assets/_59070036/Scripts/RandomCapsule.cs
+++ week-9-unity-lab/Assets/_59070036/Scripts/RandomCapsule.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class RandomCapsule : MonoBehaviour
 {
-    
+    public GameObject enemy;
+    float randX;
+    Vector3 WhereToSpawn;
+    public float spawRate = 2f;
+    float nextSpawn = 0.0f;
+
+    private void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawRate;
+            randX = Random.Range(-8.4f, 8.4f);
+            WhereToSpawn = new Vector3(randX, transform.position.y);
+            Instantiate(enemy, WhereToSpawn, Quaternion.identity);
+            
+        }
+    }
+
 }
