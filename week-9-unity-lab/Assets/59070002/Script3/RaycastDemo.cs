a2ef47524697ee0e2b27db761d04c41df3788ca9
FAHKRAM <59070002@it.kmitl.ac.th>
2019-03-17 21:09:42 +0700

    Lab Week-9 : Sence 4-5

---
+++ week-9-unity-lab/Assets/59070002/Script3/RaycastDemo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDemo : MonoBehaviour
+{
+    float rotAroundX, rotAroundY;
+
+    private void Update()
+    {
+        rotAroundY = Input.GetAxis("Mouse X");
+        rotAroundX = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotAroundX, -rotAroundY, 0);
+        DestroyObject();
+    }
+    void DestroyObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            print(hit.collider.name + " has been destroyed.");
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
