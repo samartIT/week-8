ed9c125363e5dd59f68ca4d75b313f3f414623bc
PeetTharathanakul <peet12131424@gmail.com>
2019-03-17 15:56:28 +0700

    Collision

---
+++ week-9-unity-lab/Assets/Scripts/CollisionDemo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enter: " + collision.collider.name);
+        if (collision.collider.name != "Cube3")
+            Destroy(collision.collider.gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
