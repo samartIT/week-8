547b9442f4872acdb1cd4cf8acf41825dcbfc09e
Munawang <59070036@it.kmitl.ac.th>
2019-03-17 11:42:58 +0700

    Scene4_c#

--- week-9-unity-lab/Assets/_59070036/Scripts/CollisionDemo.cs
+++ week-9-unity-lab/Assets/_59070036/Scripts/CollisionDemo.cs
@@ -7,6 +7,14 @@ public class CollisionDemo : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         print("Enter: " + collision.collider.name);
+        if (collision.collider.name != "Cube3")
+            Destroy(collision.collider.gameObject);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+    }
+    private void OnCollisionExit(Collision collision)
+    {
     }
     // Start is called before the first frame update
     void Start()
---
+++ week-9-unity-lab/Assets/_59070036/Scripts/TriggerDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDemo : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Enter: " + other.name);
+    }
+    private void OnCollisionStay(Collider other)
+    {
+        print("Trigger Stay on: " + other.name);
+        if(other.name == "Cube2")
+        {
+            Vector3 rotAng = new Vector3(0, 0, 0);
+            rotAng.y += 0.1f;
+            other.gameObject.transform.Rotate(rotAng);
+        }
+    }
+    private void OnCollisionExit(Collider other)
+    {
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
