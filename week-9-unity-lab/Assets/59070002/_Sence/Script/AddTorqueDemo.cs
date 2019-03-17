45d0505ae94a6ad51d6bb3096d44ceaa00055ff0
FAHKRAM <59070002@it.kmitl.ac.th>
2019-03-17 18:44:26 +0700

    Lab Week-9 : Sence 2 Force and Torque

---
+++ week-9-unity-lab/Assets/59070002/_Sence/Script/AddTorqueDemo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTorqueDemo : MonoBehaviour
+{
+    public float torqueForce = 100;
+    public Vector3 spinDirection = new Vector3(0,1,0);
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddTorque(spinDirection * torqueForce);
+    }
+}
