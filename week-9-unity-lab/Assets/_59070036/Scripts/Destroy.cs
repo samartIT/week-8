63cba21498f218c84b69870badb55652be30af47
Munawang <59070036@it.kmitl.ac.th>
2019-03-17 22:32:25 +0700

    Destroy Fire(capsule)

---
+++ week-9-unity-lab/Assets/_59070036/Scripts/Destroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enter: " + collision.collider.name);
+        if (collision.collider.name != "Floor" && collision.collider.name != "Wall1"
+            && collision.collider.name != "Wall2" && collision.collider.name != "Wall3"
+            && collision.collider.name != "Wall4")
+            Destroy(collision.collider.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- week-9-unity-lab/Assets/_59070036/Scripts/RandomCapsule.cs
+++ week-9-unity-lab/Assets/_59070036/Scripts/RandomCapsule.cs
@@ -4,50 +4,5 @@ using UnityEngine;
 
 public class RandomCapsule : MonoBehaviour
 {
-    //Spawn this object
-    public GameObject spawnObject;
-
-    public float maxTime = 5;
-    public float minTime = 2;
-
-    //current time
-    private float time;
-
-    //The time to spawn the object
-    private float spawnTime;
-
-    void Start()
-    {
-        SetRandomTime();
-        time = minTime;
-    }
-
-    void FixedUpdate()
-    {
-
-        //Counts up
-        time += Time.deltaTime;
-
-        //Check if its the right time to spawn the object
-        if (time >= spawnTime)
-        {
-            SpawnObject();
-            SetRandomTime();
-        }
-
-    }
-
-
-    //Spawns the object and resets the time
-    void SpawnObject()
-    {
-        time = minTime;
-        Instantiate(spawnObject, transform.position, spawnObject.transform.rotation);
-    }
-
-    //Sets the random time between minTime and maxTime
-    void SetRandomTime()
-    {
-        spawnTime = Random.Range(minTime, maxTime);
-    }
+    
 }
