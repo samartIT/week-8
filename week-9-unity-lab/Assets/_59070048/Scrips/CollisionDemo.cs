90e576ef3ad181a0613ea5ae1292bcfdc57d2977
Nattakit-Amonjarussang <59070048@kmitl.ac.th>
2019-03-17 22:23:01 +0700

    Scene4

---
+++ week-9-unity-lab/Assets/_59070048/Scrips/CollisionDemo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enter: " + collision.collider.name);
+        if (collision.collider.name != "Cube3")
+            Destroy(collision.collider.gameObject);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        print("Stay: " + collision.collider.name);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        print("Exit: " + collision.collider.name);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ week-9-unity-lab/Assets/_59070048/Scrips/TriggerDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDemo : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Enter: " + other.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("Trigger Stay on: " + other.name);
+        if(other.name == "Cube2")
+        {
+            Vector3 rotAng = new Vector3(0, 0, 0);
+            rotAng.y += 0.1f;
+            other.gameObject.transform.Rotate(rotAng);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("Trigger Exit: " + other.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
