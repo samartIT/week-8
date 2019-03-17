db7c97925a52a4a9337375e13cea04e39753dc17
Munawang <59070036@it.kmitl.ac.th>
2019-03-17 11:59:09 +0700

    Scene5_RaycastDemo.cs

---
+++ week-9-unity-lab/Assets/_59070036/Scripts/RaycastDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    float rotAroundX, rotAroundY;
+    void Update()
+    {
+        rotAroundY = Input.GetAxis("Mouse X");
+        rotAroundX = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotAroundX, -rotAroundY, 0);
+    }
+}
