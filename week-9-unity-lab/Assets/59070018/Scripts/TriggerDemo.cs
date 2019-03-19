eaf29e2308678384d361f3cafd7b0d1b3696518b
kornninikkon <59070018@it.kmitl.ac.th>
2019-03-19 23:28:40 +0700

    Scene4

---
+++ week-9-unity-lab/Assets/59070018/Scripts/TriggerDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDemo : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Enter: " + other.name);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        print("Trigger Stay on: " + other.name);
+        if (other.name == "Cube2")
+        {
+            Vector3 rotAng = new Vector3(0, 0, 0);
+            rotAng.y += 0.1f;
+            other.gameObject.transform.Rotate(rotAng);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
