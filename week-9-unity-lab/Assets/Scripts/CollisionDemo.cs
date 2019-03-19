4a7a6221e580879578f1fef56f6b3f6a175825e6
SudSan <59070001@kmitl.ac.th>
2019-03-19 23:37:19 +0700

    scene4_incomplete

---
+++ week-9-unity-lab/Assets/Scripts/CollisionDemo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo.cs : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+{
+    print("Enter: " + collision.collider.name);
+
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
