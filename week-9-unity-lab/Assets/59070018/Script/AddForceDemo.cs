7c5793fa25b7051993f1b411aa1aed25846e5b0b
kornninikkon <59070018@it.kmitl.ac.th>
2019-03-19 23:21:11 +0700

    Script

---
+++ week-9-unity-lab/Assets/59070018/Script/AddForceDemo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceDemo : MonoBehaviour
+{
+    public Vector3 pushDirecrion = new Vector3(0, 0, 1);
+    public float pushForce = 500;
+    private void OnMouseOver()
+    {
+        GetComponent<Rigidbody>().AddForce(pushDirecrion * pushForce);
+    }
+}
