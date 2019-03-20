ce4475c31ff6f165d248b9a304968166d0f9dcae
aDayyY <59070152@it.kmitl.ac.th>
2019-03-20 10:45:28 +0700

    week10

---
+++ week-9-unity-lab/Assets/_59070152/Scripts/OrbitCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float rotationSpeed = 1.5f;
+    private float _rotationY;
+    private Vector3 _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotationY = transform.eulerAngles.y;
+        _offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        if (horInput != 0)
+        {
+            _rotationY += horInput * rotationSpeed;
+        } else
+        {
+            _rotationY += Input.GetAxis("Mouse X") * rotationSpeed * 3;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, _rotationY, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+}
---
+++ week-9-unity-lab/Assets/_59070152/Scripts/RelativeMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 15.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
+    private CharacterController _charCtrl;
+    private Animator _animator;
+
+    void Start()
+    {
+        _vertSpeed = minFall;
+        _charCtrl = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if(horizontalInput != 0 || verticalInput != 0)
+        {
+            movement.x = horizontalInput * moveSpeed;
+            movement.z = verticalInput * moveSpeed;
+
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+
+            movement = target.TransformDirection(movement);
+
+            target.rotation = tmp;
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotationSpeed * Time.deltaTime);
+        }
+
+        _animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (_charCtrl.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _vertSpeed = jumpSpeed;
+                _animator.SetBool("Jumping", true);
+            } else
+            {
+                _vertSpeed = minFall;
+                _animator.SetBool("Jumping", false);
+            }
+        } else
+        {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if(_vertSpeed < terminalVelocity)
+            {
+                _vertSpeed = terminalVelocity;
+            }
+        }
+
+        movement.y = _vertSpeed;
+        movement *= Time.deltaTime;
+        _charCtrl.Move(movement);
+    }
+}
