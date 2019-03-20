94578e7fadb754350ddaac265c5e027b96fb00b5
Thanpisit Wattanasomvong <59070079@it.kmitl.ac.th>
2019-03-20 10:54:11 +0700

    commit

--- week-9-unity-lab/Assets/Scenes/RelativeMomeantion.cs
+++ week-9-unity-lab/Assets/Scenes/RelativeMomeantion.cs
@@ -7,11 +7,20 @@ public class RelativeMomeantion : MonoBehaviour
 {
     [SerializeField] private Transform target;
     public float moveSpeed = 5.0f;
+    public float rotateSpeed = 15.0f;
     private CharacterController _charCtrl;
+
+    //jump_force
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelrocity = -10.0f;
+    public float minFall = -1.5f;
+    private float _vertSpeed;
 
     void Start()
     {
         _charCtrl = GetComponent<CharacterController>();
+        _vertSpeed = minFall;
     }
 
     void Update()
@@ -19,6 +28,51 @@ public class RelativeMomeantion : MonoBehaviour
         Vector3 movement = Vector3.zero;
 
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vectical");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            movement.x = horizontalInput * moveSpeed;
+            movement.z = verticalInput * moveSpeed;
+
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+
+            movement = target.TransformDirection(movement);
+
+            target.rotation = tmp;
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotateSpeed * Time.deltaTime);
+        }
+
+        
+
+        //jump begin
+        print(_charCtrl.isGrounded);
+        if (_charCtrl.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                _vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                _vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelrocity)
+            {
+                _vertSpeed = terminalVelrocity;
+            }
+        }
+        movement.y = _vertSpeed;
+        //jump end
+
+        movement *= Time.deltaTime;
+        _charCtrl.Move(movement);
     }
 }
