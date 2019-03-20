c8843dacf7aef1e172d63c8f967e504a925dc10d
Nattakit-Amonjarussang <59070048@kmitl.ac.th>
2019-03-20 11:05:19 +0700

    W10 p1

---
+++ week-9-unity-lab/Assets/_59070048/Scrips/RelativeMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float moveSpeed = 5.0f;
+    private CharacterController _charCtrl;
+
+    void Start()
+    {
+        _charCtrl = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if(horizontalInput != 0 || verticalInput != 0)
+        {
+            movement.x = horizontalInput * moveSpeed;
+            movement.z = verticalInput * moveSpeed;
+
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+
+            movement = target.TransformDirection(movement);
+
+            target.rotation = tmp;
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+        movement *= Time.deltaTime;
+        _charCtrl.Move(movement);
+    }
+}
