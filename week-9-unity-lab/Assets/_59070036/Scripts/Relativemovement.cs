63a8ba534ca7524c932f45af530656785367d10a
Munawang <59070036@it.kmitl.ac.th>
2019-03-20 10:39:21 +0700

    WK10_TPS

---
+++ week-9-unity-lab/Assets/_59070036/Scripts/Relativemovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Relativemovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 15.0f;
+    private CharacterController _charCtrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charCtrl = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if(horizontalInput != 0 || verticalInput != 0)
+        {
+            movement.x = horizontalInput * moveSpeed;
+            movement.y = verticalInput * moveSpeed;
+
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+
+            movement = target.TransformDirection(movement);
+
+            target.rotation = tmp;
+            //transform.rotation = Quaternion.LookRotation(movement);
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotationSpeed * Time.deltaTime);
+
+            target.rotation = tmp;
+            //transform.rotation = Quaternion.LookRotation(movement);\
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotationSpeed * Time.deltaTime);
+
+        }
+        movement *= Time.deltaTime;
+        _charCtrl.Move(movement);
+    }
+}
