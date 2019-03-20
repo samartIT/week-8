9f55ab571ccd39ba40afb1b9afbfd804f3d1ddd7
Thanpisit Wattanasomvong <59070079@it.kmitl.ac.th>
2019-03-20 10:17:23 +0700

    commit

---
+++ week-9-unity-lab/Assets/Scenes/OrditCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrditCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float rotateSpeed = 1.5f;
+    private float _rotateY;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _rotateY = transform.eulerAngles.y;
+        _offset = target.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+
+        if (horInput!= 0)
+        {
+            _rotateY += horInput * rotateSpeed;
+        }
+        else
+        {
+            _rotateY += Input.GetAxis("Mouse X") * rotateSpeed * 3;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, _rotateY, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+
+}
---
+++ week-9-unity-lab/Assets/Scenes/RelativeMomeantion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMomeantion : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float moveSpeed = 5.0f;
+    private CharacterController _charCtrl;
+
+    void Start()
+    {
+        _charCtrl = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vectical");
+    }
+}
