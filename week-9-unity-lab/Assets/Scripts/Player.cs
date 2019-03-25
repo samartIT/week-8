c259e633210ccd0d8b06d22656a466db2f1a7f9d
SudSan <59070001@kmitl.ac.th>
2019-03-25 22:13:17 +0700

    week 9 Challenge! LATE!!!!!

---
+++ week-9-unity-lab/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float rotAroundX = Input.GetAxis("Horizontal");
+        float rotAroundY = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(rotAroundX, 0.0f, rotAroundY);
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrier"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
