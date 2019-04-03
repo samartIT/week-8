5452f08e68c9527695b63ad5c15eab728d407380
Thanpisit Wattanasomvong <59070079@it.kmitl.ac.th>
2019-04-03 10:46:38 +0700

    cc

---
+++ week-9-unity-lab/Assets/Scripts/RayShooter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size/4;
+        float posY = _camera.pixelHeight / 2 - size/2;  
+        GUI.Label(new Rect(posX, posY, size, size), "*","red");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float posX = _camera.pixelWidth / 2;
+            float posY = _camera.pixelHeight / 2;
+            Vector3 point = new Vector3(posX, posY, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {                
+                StartCoroutine(ShpereIndicator(hit.point));
+            }
+        }
+    }
+
+    IEnumerator ShpereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+}
---
+++ week-9-unity-lab/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactiveTarget : MonoBehaviour
+{
+    public void ReacToHit()
+    {
+        StartCoroutine(Die());
+    }
+
+    IEnumerator Die()
+    {
+        transform.Rotate(-80, 0, 0);
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }    
+}
